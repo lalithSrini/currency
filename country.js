404f2e02a5949ad7190a8ff52429ea138656df4e
lalithSrini <lalithsrinivas.t@gmail.com>
2025-03-11 12:21:38 +0530

    first commit

---
+++ country.js
@@ -0,0 +1,271 @@
+// const countryCode = {
+//   "Afghanistan": "AFN",
+//   "Albania": "ALL",
+//   "Algeria": "DZD",
+//   "Andorra": "EUR",
+//   "Angola": "AOA",
+//   "Argentina": "ARS",
+//   "Armenia": "AMD",
+//   "Australia": "AUD",
+//   "Austria": "EUR",
+//   "Azerbaijan": "AZN",
+//   "Bahamas": "BSD",
+//   "Bahrain": "BHD",
+//   "Bangladesh": "BDT",
+//   "Barbados": "BBD",
+//   "Belarus": "BYN",
+//   "Belgium": "EUR",
+//   "Belize": "BZD",
+//   "Benin": "XOF",
+//   "Bhutan": "BTN",
+//   "Bolivia": "BOB",
+//   "Bosnia and Herzegovina": "BAM",
+//   "Botswana": "BWP",
+//   "Brazil": "BRL",
+//   "Brunei": "BND",
+//   "Bulgaria": "BGN",
+//   "Burkina Faso": "XOF",
+//   "Burundi": "BIF",
+//   "Cambodia": "KHR",
+//   "Cameroon": "XAF",
+//   "Canada": "CAD",
+//   "Central African Republic": "XAF",
+//   "Chile": "CLP",
+//   "China": "CNY",
+//   "Colombia": "COP",
+//   "Costa Rica": "CRC",
+//   "Croatia": "EUR",
+//   "Cuba": "CUP",
+//   "Cyprus": "EUR",
+//   "Czech Republic": "CZK",
+//   "Denmark": "DKK",
+//   "Djibouti": "DJF",
+//   "Dominican Republic": "DOP",
+//   "Ecuador": "USD",
+//   "Egypt": "EGP",
+//   "El Salvador": "USD",
+//   "Estonia": "EUR",
+//   "Eswatini": "SZL",
+//   "Ethiopia": "ETB",
+//   "Fiji": "FJD",
+//   "Finland": "EUR",
+//   "France": "EUR",
+//   "Gabon": "XAF",
+//   "Germany": "EUR",
+//   "Ghana": "GHS",
+//   "Greece": "EUR",
+//   "Guatemala": "GTQ",
+//   "Haiti": "HTG",
+//   "Honduras": "HNL",
+//   "Hungary": "HUF",
+//   "Iceland": "ISK",
+//   "India": "INR",
+//   "Indonesia": "IDR",
+//   "Iran": "IRR",
+//   "Iraq": "IQD",
+//   "Ireland": "EUR",
+//   "Israel": "ILS",
+//   "Italy": "EUR",
+//   "Japan": "JPY",
+//   "Jordan": "JOD",
+//   "Kazakhstan": "KZT",
+//   "Kenya": "KES",
+//   "Kuwait": "KWD",
+//   "Lebanon": "LBP",
+//   "Malaysia": "MYR",
+//   "Mexico": "MXN",
+//   "Nepal": "NPR",
+//   "Netherlands": "EUR",
+//   "New Zealand": "NZD",
+//   "Nigeria": "NGN",
+//   "Norway": "NOK",
+//   "Pakistan": "PKR",
+//   "Philippines": "PHP",
+//   "Poland": "PLN",
+//   "Portugal": "EUR",
+//   "Qatar": "QAR",
+//   "Russia": "RUB",
+//   "Saudi Arabia": "SAR",
+//   "Singapore": "SGD",
+//   "South Africa": "ZAR",
+//   "South Korea": "KRW",
+//   "Spain": "EUR",
+//   "Sri Lanka": "LKR",
+//   "Sweden": "SEK",
+//   "Switzerland": "CHF",
+//   "Turkey": "TRY",
+//   "United Arab Emirates": "AED",
+//   "United Kingdom": "GBP",
+//   "United States": "USD",
+//   "Vietnam": "VND",
+//   "Zimbabwe": "ZWL"
+// };
+
+
+const countryCode =
+{
+  "Afghanistan": {
+    "currency": "AFN",
+    "flag": "https://flagsapi.com/AF/flat/64.png"
+  },
+  "Albania": {
+    "currency": "ALL",
+    "flag": "https://flagsapi.com/AL/flat/64.png"
+  },
+  "Algeria": {
+    "currency": "DZD",
+    "flag": "https://flagsapi.com/DZ/flat/64.png"
+  },
+  "Andorra": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/AD/flat/64.png"
+  },
+  "Angola": {
+    "currency": "AOA",
+    "flag": "https://flagsapi.com/AO/flat/64.png"
+  },
+  "Argentina": {
+    "currency": "ARS",
+    "flag": "https://flagsapi.com/AR/flat/64.png"
+  },
+  "Armenia": {
+    "currency": "AMD",
+    "flag": "https://flagsapi.com/AM/flat/64.png"
+  },
+  "Australia": {
+    "currency": "AUD",
+    "flag": "https://flagsapi.com/AU/flat/64.png"
+  },
+  "Austria": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/AT/flat/64.png"
+  },
+  "Azerbaijan": {
+    "currency": "AZN",
+    "flag": "https://flagsapi.com/AZ/flat/64.png"
+  },
+  "Bahrain": {
+    "currency": "BHD",
+    "flag": "https://flagsapi.com/BH/flat/64.png"
+  },
+  "Bangladesh": {
+    "currency": "BDT",
+    "flag": "https://flagsapi.com/BD/flat/64.png"
+  },
+  "Belgium": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/BE/flat/64.png"
+  },
+  "Brazil": {
+    "currency": "BRL",
+    "flag": "https://flagsapi.com/BR/flat/64.png"
+  },
+  "Canada": {
+    "currency": "CAD",
+    "flag": "https://flagsapi.com/CA/flat/64.png"
+  },
+  "China": {
+    "currency": "CNY",
+    "flag": "https://flagsapi.com/CN/flat/64.png"
+  },
+  "Denmark": {
+    "currency": "DKK",
+    "flag": "https://flagsapi.com/DK/flat/64.png"
+  },
+  "Egypt": {
+    "currency": "EGP",
+    "flag": "https://flagsapi.com/EG/flat/64.png"
+  },
+  "Finland": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/FI/flat/64.png"
+  },
+  "France": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/FR/flat/64.png"
+  },
+  "Germany": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/DE/flat/64.png"
+  },
+  "India": {
+    "currency": "INR",
+    "flag": "https://flagsapi.com/IN/flat/64.png"
+  },
+  "Indonesia": {
+    "currency": "IDR",
+    "flag": "https://flagsapi.com/ID/flat/64.png"
+  },
+  "Iran": {
+    "currency": "IRR",
+    "flag": "https://flagsapi.com/IR/flat/64.png"
+  },
+  "Iraq": {
+    "currency": "IQD",
+    "flag": "https://flagsapi.com/IQ/flat/64.png"
+  },
+  "Italy": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/IT/flat/64.png"
+  },
+  "Japan": {
+    "currency": "JPY",
+    "flag": "https://flagsapi.com/JP/flat/64.png"
+  },
+  "Mexico": {
+    "currency": "MXN",
+    "flag": "https://flagsapi.com/MX/flat/64.png"
+  },
+  "Netherlands": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/NL/flat/64.png"
+  },
+  "Norway": {
+    "currency": "NOK",
+    "flag": "https://flagsapi.com/NO/flat/64.png"
+  },
+  "Pakistan": {
+    "currency": "PKR",
+    "flag": "https://flagsapi.com/PK/flat/64.png"
+  },
+  "Russia": {
+    "currency": "RUB",
+    "flag": "https://flagsapi.com/RU/flat/64.png"
+  },
+  "Saudi Arabia": {
+    "currency": "SAR",
+    "flag": "https://flagsapi.com/SA/flat/64.png"
+  },
+  "South Korea": {
+    "currency": "KRW",
+    "flag": "https://flagsapi.com/KR/flat/64.png"
+  },
+  "Spain": {
+    "currency": "EUR",
+    "flag": "https://flagsapi.com/ES/flat/64.png"
+  },
+  "Sweden": {
+    "currency": "SEK",
+    "flag": "https://flagsapi.com/SE/flat/64.png"
+  },
+  "Switzerland": {
+    "currency": "CHF",
+    "flag": "https://flagsapi.com/CH/flat/64.png"
+  },
+  "Turkey": {
+    "currency": "TRY",
+    "flag": "https://flagsapi.com/TR/flat/64.png"
+  },
+  "United Kingdom": {
+    "currency": "GBP",
+    "flag": "https://flagsapi.com/GB/flat/64.png"
+  },
+  "United States": {
+    "currency": "USD",
+    "flag": "https://flagsapi.com/US/flat/64.png"
+  },
+  "Vietnam": {
+    "currency": "VND",
+    "flag": "https://flagsapi.com/VN/flat/64.png"
+  }
+}
